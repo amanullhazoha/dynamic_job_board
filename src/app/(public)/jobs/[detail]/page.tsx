d8035226b7120c402c92861d94926b872c1c4c5a
Amanullha Zoha <amanullhazoha3784@gmail.com>
2024-12-06 21:15:30 +0600

    job page render issue solve

--- src/app/(public)/jobs/[detail]/page.tsx
+++ src/app/(public)/jobs/[detail]/page.tsx
@@ -1,11 +1,15 @@
 import { getJob } from "@/app/services";
+import { notFound } from "next/navigation";
 import JobDetailPage from "@/view/job/JobDetailPage";
-// import { jobDetails } from "@/utilities/interface/job.interface";
 
 const JobDetail = async ({ params }: { params: any }) => {
   const job: any = await getJob(params?.detail);
 
-  return <JobDetailPage jobDetail={job} />;
+  if (!job?.data) {
+    notFound();
+  }
+
+  return <JobDetailPage jobDetail={job?.data} />;
 };
 
 export default JobDetail;
